1c113a5031063ed2c8aac3b81218d06745c60d6a
rebersincar <rebersincar07@gmail.com>
2021-08-05 14:25:04 +0300

    socket.io version downgraded to 2.3.0

--- server.js
+++ server.js
@@ -1,36 +1,19 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server).of('/Chat');
+
+const PORT = 8080 || process.env.PORT;
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 // run when client connects
 io.on('connection', socket => {
-    console.log(socket.request);
-    socket.on('join-room', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room);
-        socket.join(user.room);
-
-        socket.to(user.room).emit('message', user.username + ' has joined the chat');
-        io.to(user.room).emit('user-room', getRoomUsers(user.room));
-    });
-
-    socket.on("chat-message", msg => {
-        const user = getCurrentUser(socket.id);
-        io.to(user.room).emit('chat-message', formatMessage(user.username, msg));
-    });
-
+    console.log('a user connected');
     socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
-        if (user) {
-            io.to(user.room).emit('message', user.username + ' has left the chat');
-            io.to(user.room).emit('user-room', getRoomUsers(user.room));
-        }
+        console.log('a user disconnected');
     });
 });
-
-const PORT = 8080 || process.env.PORT;
 
 server.listen(PORT, () => console.log("Server running on port " + PORT));
